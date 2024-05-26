7af5943f281e8a7e056b07effb3c8fc1dc5911c8
Guilherme <guilhermehenrique12308@gmail.com>
2024-05-26 16:11:23 -0300

    feat: create server

---
+++ connectify-api/src/env/index.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+const schemaEnv = z.object({
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = schemaEnv.safeParse(process.env);
+
+if (_env.success === false) {
+  console.log("Error environment not passed", _env.error.format);
+
+  throw new Error();
+}
+
+export const env = _env.data;
