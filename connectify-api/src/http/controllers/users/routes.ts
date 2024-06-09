e1003f201d8ba2f691d9756e2a2731936a8ee54f
Guilherme <guilhermehenrique12308@gmail.com>
2024-06-08 22:19:17 -0300

    feat: add fetch controller in routes

--- connectify-api/src/http/controllers/users/routes.ts
+++ connectify-api/src/http/controllers/users/routes.ts
@@ -7,12 +7,14 @@ import { search } from "./search";
 import { profile } from "./profile";
 import { authentificate } from "./authentificate";
 import { edit } from "./edit";
+import { fetch } from "./fetch";
 
 export async function routesUser(app: FastifyInstance) {
   app.post("/user", register);
   app.post("/session", authentificate);
 
-  app.get("/me", { onRequest: verifyJwt }, profile);
-  app.get("/users", { onRequest: verifyJwt }, search);
   app.put("/user", { onRequest: verifyJwt }, edit);
+  app.get("/me", { onRequest: verifyJwt }, profile);
+  app.get("/users/search", { onRequest: verifyJwt }, search);
+  app.get("/users/fetch", { onRequest: verifyJwt }, fetch);
 }
